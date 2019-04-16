882962bb3f1a02459a93d695a0d6f30e31c0acff
Romuald Bulyshko <romuald@bulyshko.com>
2019-04-16 21:44:35 +0300

    Terminate gracefully

--- src/server.js
+++ src/server.js
@@ -9,3 +9,11 @@ const server = app.listen(PORT)
 module.exports = server
 
 const wss = require('./websocket-server')(server)
+
+;['SIGTERM', 'SIGINT'].forEach(event => {
+  process.on(event, () => {
+    console.info('%s signal received.', event)
+    wss.clients.forEach(client => client.close())
+    process.exit(0)
+  })
+})
