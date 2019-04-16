c4caaef37900320e77c8cc4967467a857ea46c19
Romuald Bulyshko <romuald@bulyshko.com>
2019-04-16 22:58:27 +0300

    Store usernames in lowercase

--- src/db.js
+++ src/db.js
@@ -1,15 +1,15 @@
 const users = []
 
 function isUsernameTaken (username) {
-  return users.includes(username)
+  return users.includes(username.toLowerCase())
 }
 
 function takeUsername (username) {
-  users.push(username)
+  users.push(username.toLowerCase())
 }
 
 function releaseUsername (username) {
-  users.splice(users.indexOf(username), 1)
+  users.splice(users.indexOf(username.toLowerCase()), 1)
 }
 
 module.exports = { isUsernameTaken, takeUsername, releaseUsername }
